0c1cc9db4a37c5a216a3577c71714e938d6bf8e2
chihaya_anon <chihaya_anon_0908@outlook.com>
2025-06-08 12:49:54 +0800

    feat: generate mvc frame

--- src/test/gen_test.go
+++ src/test/gen_test.go
@@ -27,12 +27,27 @@ func Test_MVC(t *testing.T) {
 }
 func Test_GenController(t *testing.T) {
 	gen.GenerateGinController("users_orchestrator", "section", "test", []string{"test_a"})
+	gen.GenerateGinController("users_orchestrator", "section", "auth", []string{"auth"})
+	gen.GenerateGinController("users_orchestrator", "section", "user", []string{"user"})
+	gen.GenerateGinController("users_orchestrator", "section", "security", []string{"security"})
+	gen.GenerateGinController("users_orchestrator", "section", "session", []string{"session"})
+	gen.GenerateGinController("users_orchestrator", "section", "audit", []string{"audit"})
 }
 
 func Test_GenService(t *testing.T) {
 	gen.GenerateService("users_orchestrator", "section", "test", []string{"test_a"})
+	gen.GenerateService("users_orchestrator", "section", "auth", []string{"auth"})
+	gen.GenerateService("users_orchestrator", "section", "user", []string{"user"})
+	gen.GenerateService("users_orchestrator", "section", "security", []string{"security"})
+	gen.GenerateService("users_orchestrator", "section", "session", []string{"session"})
+	gen.GenerateService("users_orchestrator", "section", "audit", []string{"audit"})
 }
 
 func Test_GenMapper(t *testing.T) {
 	gen.GenerateMapper("users_orchestrator", "section", "test", []string{"test_a"})
+	gen.GenerateMapper("users_orchestrator", "section", "auth", []string{"auth"})
+	gen.GenerateMapper("users_orchestrator", "section", "user", []string{"user"})
+	gen.GenerateMapper("users_orchestrator", "section", "security", []string{"security"})
+	gen.GenerateMapper("users_orchestrator", "section", "session", []string{"session"})
+	gen.GenerateMapper("users_orchestrator", "section", "audit", []string{"audit"})
 }
